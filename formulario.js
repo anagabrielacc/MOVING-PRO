4a6aa9f2ea470a6188cbd6b49639516677a0c9fd
anagabrielacc <soyanagabriela08@gmail.com>
2025-03-06 23:23:13 -0500

    Realización del formulario con JavaScript - (Local Storage)

---
+++ formulario.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('form').addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        let name = document.getElementById('name').value;
+        let cedula = document.getElementById('cedula').value;
+        let email = document.getElementById('email').value;
+        let servicio = document.getElementById('servicio').value;
+        let day = document.getElementById('day').value;
+        let month = document.getElementById('month').value;
+        let year = document.getElementById('year').value;
+        let observaciones = document.getElementById('Observaciones').value;
+
+        let datosFormulario = {
+            nombre: name,
+            cedula: cedula,
+            email: email,
+            servicio: servicio,
+            fechaMudanza: `${day} ${month} ${year}`,
+            observaciones: observaciones
+        };
+
+        localStorage.setItem('formularioDatos', JSON.stringify(datosFormulario));
+
+        alert('Información guardada correctamente en Local Storage');
+
+        this.reset();
+    });
+});
